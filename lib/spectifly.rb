416fcf73b0f23882344ee0917b31195046d1a113
Ravi Gadad <ravi@renewfund.com>
2013-05-01 16:29:23 -0700

    Require all base files by default (but not Xsd)

--- lib/spectifly.rb
+++ lib/spectifly.rb
@@ -1,7 +1,8 @@
 require_relative 'spectifly/support'
 require_relative 'spectifly/entity'
 require_relative 'spectifly/builder'
-require_relative 'spectifly/xsd/builder'
+require_relative 'spectifly/field'
+require_relative 'spectifly/types'
 require_relative 'spectifly/version'
 
 module Spectifly
